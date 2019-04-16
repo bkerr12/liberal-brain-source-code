d0e36622329c2397f8f672c360e7c70f0836daae
bkerr12 <49648765+bkerr12@users.noreply.github.com>
2019-04-16 10:00:28 -0500

    Create general_media_handler.js

---
+++ general_media_handler.js
@@ -0,0 +1,23 @@
+var debates_won = 0;
+var iq = -100;
+var ignorance = 100%;
+
+while (debate_with_right == true){
+print "racist";
+print "bigot";
+print "nazi";
+}
+
+if (opponent == white){
+print "stop supporting white supremacy";
+
+else{
+print "i love the diversity in this country";
+}
+
+
+if (article == conservative){
+print ("fascist");
+print ("fake news");
+print ("this makes me mad");
+}
