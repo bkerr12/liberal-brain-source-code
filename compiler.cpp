44c182602e2a4a8e2cd01785b9eafeca50572eb0
bkerr12 <49648765+bkerr12@users.noreply.github.com>
2019-04-16 10:02:54 -0500

    Create compiler.cpp

---
+++ compiler.cpp
@@ -0,0 +1,111 @@
+//translates words used by opponents 
+#include <iostream>   
+using namespace std;
+ int main() 
+{
+
+$responses {
+> racist;
+> sexist;
+> bigot;
+> nazi;
+> white supremacist;
+> toxic masculinity;
+> homophobe;
+> diversity hater;
+> mentally disabled;
+> retard;
+> offensive;
+> serial killer;
+}
+
+if (word = white) {
+white = racist;
+setMood > angry;
+print ("your" +response+);
+}
+
+if (word = truth){
+truth = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = disagree){
+disagree = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = religion){
+religion = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = anti abortion){
+anti abortion = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = gun){
+gun = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = terrorist){
+terrorist = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = muslim){
+muslim = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = opinion){
+opinion = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = peaceful protest){
+peaceful protest = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = trump){
+trump = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = isis){
+isis = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = african american){
+african american = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = hello how are you){
+hello how are you = racist;
+setMood > angry;
+print ("your" +response+)
+}
+
+if (word = //insert word here){
+//word = racist;
+setMood > angry;
+print ("your" +response+)
+   }
+}
